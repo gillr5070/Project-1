28d94b42bb68220955fbd58aaa77862ffb3069d3
gillr5070 <153116066+gillr5070@users.noreply.github.com>
2023-12-06 13:52:49 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example JavaScript interaction (can be expanded)
+// For instance, a simple alert on page load
+window.onload = function() {
+    alert("Welcome to Barbour Cutting Designs!");
+  };
+  
